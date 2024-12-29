db2aa63915ed5fc8d8564de22e92c3be431938b0
ALVES Alexis <alexis.alves@etu.uca.fr>
2024-12-29 15:14:27 +0100

    Remove Docker Compose and init.sql because of passwords

--- docker/init.sql
+++
@@ -1,9 +0,0 @@
--- Initialisation de la base de données MariaDB avec 2 utilisateurs et une base de données.
-
-CREATE USER 'nextcloud_u'@'%' IDENTIFIED BY 'pwd';
-CREATE USER 'wordpress_u'@'%' IDENTIFIED BY 'pwd';
-CREATE DATABASE nextcloud;
-CREATE DATABASE wordpress;
-GRANT ALL PRIVILEGES ON nextcloud.* TO 'nextcloud_u'@'%';
-GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress_u'@'%';
-FLUSH PRIVILEGES;
