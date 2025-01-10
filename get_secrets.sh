94caa14569192963fa55c084a873b12449b2f252
ALVES Alexis <alexis.alves@etu.uca.fr>
2025-01-10 23:02:11 +0100

    Refactor CI/CD pipeline and add secret management for Docker deployment

--- get_secrets.sh
+++ get_secrets.sh
@@ -4,16 +4,15 @@ VAULT_ADDR=https://vault.edu.forestier.re/v1
 VAULT_TOKEN=$(curl -s -X POST -k --data "{\"token\":\"$CI_JOB_TOKEN\"}" "$VAULT_ADDR/auth/gitlab/ci" | python3 jq.py "auth/client_token")
 
 
-# Retrieve Docker Registry secret
+# Retrieve Docker Registry secret from Vault
 SECRET_DOCKER_REGISTRY=$(curl -s -X GET -k --header "X-Vault-Token: $VAULT_TOKEN" "$VAULT_ADDR/shared/data/docker_registry")
 
-# Here, export value Y of our secret to Y in our environment
 export DOCKER_REGISTRY_URL=$(echo "$SECRET_DOCKER_REGISTRY" | python3 jq.py "data/data/URL")
 export DOCKER_REGISTRY_USER=$(echo "$SECRET_DOCKER_REGISTRY" | python3 jq.py "data/data/USERNAME")
 export DOCKER_REGISTRY_PASSWORD=$(echo "$SECRET_DOCKER_REGISTRY" | python3 jq.py "data/data/PASSWORD")
 
 
-# Retrieve my secrets
+# Retrieve my secrets from Vault
 SECRET_HOME=$(curl -s -X GET -k --header "X-Vault-Token: $VAULT_TOKEN" "$VAULT_ADDR/home/data/alalves")
 
 export MARIADB_ROOT_PASSWORD=$(echo "$SECRET_HOME" | python3 jq.py "data/data/MARIADB_ROOT_PASSWORD")
@@ -21,3 +20,5 @@ export NEXTCLOUD_USER=$(echo "$SECRET_HOME" | python3 jq.py "data/data/NEXTCLOUD
 export NEXTCLOUD_PASSWORD=$(echo "$SECRET_HOME" | python3 jq.py "data/data/NEXTCLOUD_PASSWORD")
 export WORDPRESS_USER=$(echo "$SECRET_HOME" | python3 jq.py "data/data/WORDPRESS_USER")
 export WORDPRESS_PASSWORD=$(echo "$SECRET_HOME" | python3 jq.py "data/data/WORDPRESS_PASSWORD")
+export CLOUDS_YAML=$(echo "$SECRET_HOME" | python3 jq.py "data/data/CLOUDS_YAML")
+export ANSIBLE_VAULT_PASS=$(echo "$SECRET_HOME" | python3 jq.py "data/data/ANSIBLE_VAULT_PASS")
